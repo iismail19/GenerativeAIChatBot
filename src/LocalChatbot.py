56c728e8a3c1aa20631255120535cfe7bfa93c61
iismail19 <2015.ismail@gmail.com>
2023-07-23 02:19:10 -0500

    Update to use google/flan-t5-xxl embedding with HuggingFaceHub

--- src/LocalChatbot.py
+++ src/LocalChatbot.py
@@ -5,6 +5,9 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 import re
+from langchain.chains.question_answering import load_qa_chain
+from langchain import HuggingFaceHub
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 import os
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = "<ENTER_KEY_HERE>"
@@ -13,7 +16,12 @@ os.environ["HUGGINGFACEHUB_API_TOKEN"] = "<ENTER_KEY_HERE>"
 def get_pdf_text(filePath):
     loader = PyPDFLoader(filePath)
     doc = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 600,
+        chunk_overlap  = 0,
+        length_function = len,
+    )
     chunks = text_splitter.split_documents(doc)
     return chunks
 
@@ -23,8 +31,7 @@ def get_embeddings():
 
 # Create vectorstores - will now have our embedded documents
 def get_vector_stores(documents, embeddings):
-    db = FAISS.from_documents(documents, embeddings)
-    return db
+    return FAISS.from_documents(documents, embeddings)
 
 import textwrap
 
@@ -40,21 +47,29 @@ def wrap_text_preserve_newlines(text, width=200):
 
     return wrapped_text
 
+def large_language_model():
+    return HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":.05, "max_length":1024})
+
+
 def main():
-    file_path = 'src/resources/cleanCode.pdf'
+    file_path = 'src/resources/intern.pdf'
     documents = get_pdf_text(file_path)
 
     embeddings = get_embeddings()
 
     vector_stores = get_vector_stores(documents, embeddings)
 
-    # Test and ask qeustions
-    query = "What is the purpose of writing clean code?"
-    results = vector_stores.similarity_search(query)
+    # Test and ask qeustions - this is based on similarity
+    query = "How many hours are needed to complete an internship?"
+    docs = vector_stores.similarity_search(query)
     
-    print(wrap_text_preserve_newlines(str(results[0].page_content)))
+    #print(wrap_text_preserve_newlines(str(docs[0].page_content)))
 
-
+    # Integrate it as part of a Q and A chain
+    llm = large_language_model()
+    chain = load_qa_chain(llm, chain_type="stuff")
+    chain.run(input_documents=docs, question=query)
+    print(wrap_text_preserve_newlines(str(docs[0].page_content)))
 
 if __name__ == "__main__":
     main()
